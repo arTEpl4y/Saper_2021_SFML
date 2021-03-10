a4e392715de35fffcf43dc77fb673d0cbfe2b768
259032 <259032@student.pwr.edu.pl>
2021-03-10 14:15:59 +0000

    better Minesweeper

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define min(x, y) x<y ? x:y
+
 struct Field
 {
 	bool hasMine;
@@ -9,33 +11,34 @@ struct Field
 
 class MinesweeperBoard
 {
-  Field board[10][10];    // 100-elementowa tablica, 100-elementowych tablic typu Field 
+  Field board[100][100];    // 100-elementowa tablica, 100-elementowych tablic typu Field 
   int width;                // rzeczywista szerokość planszy
   int height;               // rzeczywista wysokość planszy
 
 public:
-  MinesweeperBoard();
+  MinesweeperBoard(int wiersz, int kolumna){
+    width = min(wiersz, 100);
+    height = min(kolumna, 100);
+    Field pole;
+    pole.hasMine = 0;
+    pole.hasFlag = 0;
+    pole.isRevealed = 0;
+
+    for(int x = 0; x < width; x++){
+      for(int y = 0; y < width; y++){ //based
+        board[x][y] = pole;
+      }
+    }
+
+  }
   void debug_display() const;
 };
-
-MinesweeperBoard::MinesweeperBoard(){
-  width = 10;
-  height = 10;
-
-  for(int kolumna = 0; kolumna < width; kolumna++){
-    for(int wiersz = 0; wiersz < height; wiersz++){
-      board[wiersz][kolumna].hasMine = 0;
-      board[wiersz][kolumna].hasFlag = 0;
-      board[wiersz][kolumna].isRevealed = 0;
-    }
-  }
-}
 
 void intro(){
   std::cout << "MP xxx032" << std::endl;
 }
 
-int main() {
+int main(){
   intro();
-  MinesweeperBoard();
+  MinesweeperBoard kek = MinesweeperBoard(10 , 10);
 }
